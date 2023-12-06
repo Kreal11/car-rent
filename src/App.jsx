617fe4066996c410d8892343ba78b36e5aad7203
Kreal11 <kirillshyrokov2@gmail.com>
2023-12-06 20:03:39 +0200

    styles, structure, fonts, package.json

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./pages/home/Home";
+import { Catalog } from "./pages/catalog/Catalog";
+import { Favorites } from "./pages/favorites/Favorites";
+import { Layout } from "./components/layout/Layout";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/catalog" element={<Catalog />} />
+        <Route path="/favorites" element={<Favorites />} />
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import { GlobalStyles } from "./styles/GlobalStyles";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter basename="/test-task-car-rent">
+    <React.StrictMode>
+      <App />
+      <GlobalStyles />
+      <ToastContainer autoClose={1000} theme="colored" />
+    </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
