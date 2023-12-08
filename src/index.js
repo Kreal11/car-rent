c9ddc148b3001d62b26b0df4b49be44940d541cd
Kreal11 <kirillshyrokov2@gmail.com>
2023-12-08 17:39:38 +0200

    remover persistGate

--- src/index.js
+++ src/index.js
@@ -4,9 +4,8 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import { GlobalStyles } from './styles/GlobalStyles';
-import { persistor, store } from './redux/store';
+import { store } from './redux/store';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
